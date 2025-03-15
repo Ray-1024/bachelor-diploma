886d031aee3b5c45971d9a7a95ad6224f9e274ed
Ray_1024 <skovorodnikovda@mail.ru>
2025-03-15 23:58:48 +0300

    Add problem service

---
+++ problem-service/src/test/java/ray1024/problemservice/ProblemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ray1024.problemservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProblemServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
