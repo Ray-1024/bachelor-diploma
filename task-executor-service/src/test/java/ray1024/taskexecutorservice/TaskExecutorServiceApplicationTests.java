f72788b0195f1d2b19c0d1baa5458ad51c5c3cf5
Ray_1024 <skovorodnikovda@mail.ru>
2025-03-15 23:39:33 +0300

    Add empty task executor service

---
+++ task-executor-service/src/test/java/ray1024/taskexecutorservice/TaskExecutorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ray1024.taskexecutorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskExecutorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
